ca9dfc884ff7ca636677a7dd5d5663d7574a936d
Gabriel Dias <gabrieldiasgama@hotmail.com>
2022-10-18 20:35:11 -0300

    first commit of validation

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/validationCep/:cep', async (request, response) => {
+    const cep = request.params.cep
+    const validator = await axios
+        .get(`https://busca-cep-production.up.railway.app/cep/${cep}`)
+        .then((data) => {
+            if(data && data.data && data.data?.result && !data.data.result.localidade ){
+                response.json('não existe')
+            }else{
+                response.json("existe")
+            }
+        }).catch(() => {
+            console.log('Sorry! This CEP is not valid')
+        })
+        return {validator}
+})
+
+app.listen(process.env.PORT || 3333, () => console.log('Server is running...'))
